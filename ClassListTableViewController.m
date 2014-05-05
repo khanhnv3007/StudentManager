f61a8ae406f2a50a9b6ac9ddfab1a9f46f2d88c4
khanhnv3007 <khanhnv3007@gmail.com>
2014-05-05 17:20:53 +0700

    Ok delete

--- ClassListTableViewController.m
+++ ClassListTableViewController.m
@@ -19,7 +19,7 @@
 @property (nonatomic, strong) NSMutableArray *classList;
 @property (weak, nonatomic) IBOutlet UITableView *rootTableView;
 @property (strong, nonatomic) NSString *classIsSelected;
-@property (strong, nonatomic) Subject *classIsDeleted;
+//@property (strong, nonatomic) Subject *classIsDeleted;
 
 @end
 
@@ -27,24 +27,23 @@
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-    [self addButton];
+	[self addButton];
 	[self loadAddClass];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
 	[self loadAddClass];
-    [self addButton];
+	[self addButton];
 }
 
-- (void)addButton
-{
-    AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication]delegate];
-    if (appDelegate.isAdmin) {
-        self.navigationItem.rightBarButtonItem.enabled = YES;
-    }
-    if (appDelegate.isStudent || appDelegate.isTeacher) {
-        self.navigationItem.rightBarButtonItem = nil;
-    }
+- (void)addButton {
+	AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication]delegate];
+	if (appDelegate.isAdmin) {
+		self.navigationItem.rightBarButtonItem.enabled = YES;
+	}
+	if (appDelegate.isStudent || appDelegate.isTeacher) {
+		self.navigationItem.rightBarButtonItem = nil;
+	}
 }
 
 - (void)loadAddClass {
@@ -75,13 +74,13 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
 	static NSString *cellIdentifier = @"cellClassList";
 	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
-    
+
 	Subject *subject = nil;
 	if (cell == nil) {
 		cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellIdentifier];
 	}
 	subject = (Subject *)[self.classList objectAtIndex:indexPath.row];
-    
+
 	if (subject) {
 		cell.imageView.image = [UIImage imageNamed:@"menu30.png"];
 		cell.textLabel.text = subject.name;
@@ -90,54 +89,54 @@
 	return cell;
 }
 
-- (BOOL) tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath{
-    return YES;
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+	return YES;
 }
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    [tableView deselectRowAtIndexPath:indexPath animated:NO];
-    [[Util sharedUtil] showMessage:@"Are you sure want to enroll this class" withTitle:@"Message" cancelButtonTitle:@"YES" otherButtonTitles:@"NO" delegate:self andTag:100];
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+	[tableView deselectRowAtIndexPath:indexPath animated:NO];
+	[[Util sharedUtil] showMessage:@"Are you sure want to enroll this class" withTitle:@"Message" cancelButtonTitle:@"YES" otherButtonTitles:@"NO" delegate:self andTag:100];
 }
 
-- (NSString *)getStudentID
-{
-    NSArray *student = [[DataManager sharedDataManager]getAllStudentAccount];
-    NSMutableArray *studentList = [NSMutableArray arrayWithArray:student];
-    AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication]delegate];
-    
-    for (Student *student in studentList) {
-        if ([student.username isEqual:appDelegate.username]) {
-            return student.studentID;
-        }
-    }
-    return nil;
+- (NSString *)getStudentID {
+	NSArray *student = [[DataManager sharedDataManager]getAllStudentAccount];
+	NSMutableArray *studentList = [NSMutableArray arrayWithArray:student];
+	AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication]delegate];
+
+	for (Student *student in studentList) {
+		if ([student.username isEqual:appDelegate.username]) {
+			return student.studentID;
+		}
+	}
+	return nil;
 }
 
-- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
-{
-    if (alertView.tag == 100) {
-        if (buttonIndex == 0) {
-            
-            MarkModel *markModel = [[DataManager sharedDataManager] createMarkModelWithStudentID:[self getStudentID] classID:nil Mid:nil Final:nil Average:nil];
-            NSLog(@"OK");
-            [markModel managedObject];
-        } else {
-            NSLog(@"Nothing");
-        }
-    }
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+	if (alertView.tag == 100) {
+		if (buttonIndex == 0) {
+			MarkModel *markModel = [[DataManager sharedDataManager] createMarkModelWithStudentID:[self getStudentID] classID:nil Mid:nil Final:nil Average:nil];
+			NSLog(@"OK");
+			[markModel managedObject];
+		}
+		else {
+			NSLog(@"Nothing");
+		}
+	}
 }
 
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
-    
-    [tableView beginUpdates];
-    self.classIsDeleted = (Subject*)[self.classList objectAtIndex:indexPath.row];
-    [self.classIsDeleted MR_deleteEntity];
-    [self loadAddClass];
-    [self.rootTableView reloadData];
-    //[[Util sharedUtil] showMessage:@"You Sur Delete a contact" withTitle:@"Message" cancelButtonTitle:@"Cancel" otherButtonTitles:@"Ok" delegate:self andTag:1];
-    [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    [tableView endUpdates];
-    
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+	if (editingStyle == UITableViewCellEditingStyleDelete) {
+		[tableView beginUpdates];
+        Subject *classIsDeleted = [self.classList objectAtIndex:indexPath.row];
+
+		[classIsDeleted MR_deleteEntity];
+        [[NSManagedObjectContext MR_defaultContext] MR_saveToPersistentStoreAndWait];
+		//[[Util sharedUtil] showMessage:@"You Sur Delete a contact" withTitle:@"Message" cancelButtonTitle:@"Cancel" otherButtonTitles:@"Ok" delegate:self andTag:1];
+		[tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+        [self loadAddClass];
+		[self.rootTableView reloadData];
+		[tableView endUpdates];
+	}
 }
+
 @end
