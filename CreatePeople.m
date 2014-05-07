dd34aee54ac63a4fbebca10cb34619d9da342e9b
Nguyen Nam Phong <phongnn_57@vnu.edu.vn>
2014-05-07 17:07:48 +0700

    no message

--- CreatePeople.m
+++ CreatePeople.m
@@ -44,18 +44,21 @@
         admin.name = self.name.text;
         admin.username = self.username.text;
         admin.password = self.username.text;
+        [[NSManagedObjectContext MR_defaultContext] MR_saveToPersistentStoreAndWait];
     }
     if (self.selectUser.selectedSegmentIndex == 1) {
         Teacher *teacher = [Teacher MR_createEntity];
         teacher.name = self.name.text;
         teacher.username = self.username.text;
         teacher.password = self.username.text;
+        [[NSManagedObjectContext MR_defaultContext] MR_saveToPersistentStoreAndWait];
     }
     if (self.selectUser.selectedSegmentIndex == 2) {
         Student *student = [Student MR_createEntity];
         student.name = self.name.text;
         student.username = self.username.text;
         student.password = self.username.text;
+        [[NSManagedObjectContext MR_defaultContext] MR_saveToPersistentStoreAndWait];
     }
 }
 @end
