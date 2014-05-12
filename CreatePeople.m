bd159f24639e95bb51087e5ddf4fc273c2d66c40
khanhnv3007 <khanhnv3007@gmail.com>
2014-05-12 14:52:18 +0700

    check username

--- CreatePeople.m
+++ CreatePeople.m
@@ -14,6 +14,10 @@
 
 @interface CreatePeople ()
 
+@property (nonatomic, strong) Admin *admin;
+@property (nonatomic, strong) Teacher *teacher;
+@property (nonatomic, strong) Student *student;
+
 @end
 
 @implementation CreatePeople
@@ -28,31 +32,47 @@
     [super didReceiveMemoryWarning];
 }
 
+-(BOOL)checkusername{
+    
+    self.admin = [Admin MR_findFirstByAttribute:@"username" withValue:self.username.text];
+	self.teacher = [Teacher MR_findFirstByAttribute:@"username" withValue:self.username.text];
+	self.student = [Student MR_findFirstByAttribute:@"username" withValue:self.username.text];
+    if (self.admin == nil && self.teacher == nil && self.student == nil) {
+        return YES;
+    }
+    return NO;
+}
+
 - (IBAction)createPeople:(id)sender {
-    if (self.selectUser.selectedSegmentIndex == 0) {
-        Admin *admin = [Admin MR_createEntity];
-        admin.name = self.name.text;
-        admin.username = self.username.text;
-        admin.password = self.username.text;
-        [[NSManagedObjectContext MR_defaultContext] MR_saveToPersistentStoreAndWait];
-        [[Util sharedUtil] showMessage:@"An admin has been created!" withTitle:@"Create Success"];
+    if ([self checkusername]) {
+        if (self.selectUser.selectedSegmentIndex == 0) {
+            Admin *admin = [Admin MR_createEntity];
+            admin.name = self.name.text;
+            admin.username = self.username.text;
+            admin.password = self.username.text;
+            [[NSManagedObjectContext MR_defaultContext] MR_saveToPersistentStoreAndWait];
+            [[Util sharedUtil] showMessage:@"An admin has been created!" withTitle:@"Create Success"];
+        }
+        if (self.selectUser.selectedSegmentIndex == 1) {
+            Teacher *teacher = [Teacher MR_createEntity];
+            teacher.name = self.name.text;
+            teacher.username = self.username.text;
+            teacher.password = self.username.text;
+            [[NSManagedObjectContext MR_defaultContext] MR_saveToPersistentStoreAndWait];
+            [[Util sharedUtil] showMessage:@"A teacher has been created!" withTitle:@"Create Success"];
+        }
+        if (self.selectUser.selectedSegmentIndex == 2) {
+            Student *student = [Student MR_createEntity];
+            student.name = self.name.text;
+            student.username = self.username.text;
+            student.password = self.username.text;
+            [[NSManagedObjectContext MR_defaultContext] MR_saveToPersistentStoreAndWait];
+            [[Util sharedUtil] showMessage:@"A student has been created!" withTitle:@"Create Success"];
+        }
+    } else{
+        [[Util sharedUtil] showMessage:@"Username is in use" withTitle:@"Create failed!"];
     }
-    if (self.selectUser.selectedSegmentIndex == 1) {
-        Teacher *teacher = [Teacher MR_createEntity];
-        teacher.name = self.name.text;
-        teacher.username = self.username.text;
-        teacher.password = self.username.text;
-        [[NSManagedObjectContext MR_defaultContext] MR_saveToPersistentStoreAndWait];
-        [[Util sharedUtil] showMessage:@"A teacher has been created!" withTitle:@"Create Success"];
-    }
-    if (self.selectUser.selectedSegmentIndex == 2) {
-        Student *student = [Student MR_createEntity];
-        student.name = self.name.text;
-        student.username = self.username.text;
-        student.password = self.username.text;
-        [[NSManagedObjectContext MR_defaultContext] MR_saveToPersistentStoreAndWait];
-        [[Util sharedUtil] showMessage:@"A student has been created!" withTitle:@"Create Success"];
-    }
+    
     
 }
 
